c546f09d6acaa601f38f7326436c9d13009eb491
Ethan <N/A>
2019-03-20 14:09:31 -0700

    Commit 4 - Fizz Buzz answered

--- js/app.js
+++ js/app.js
@@ -24,3 +24,23 @@ for(let i = 0; i<=20;i++){
             }
     }
 }
+
+//Commit 4
+//Commit 1
+for(let i = 0; i<=100;i++){
+    
+    if(i%3==0&&i%5==0)
+    {
+        console.log("FizzBuzz");
+    }
+    else if(i%3==0){
+        console.log("Fizz");
+    }
+    else if(i%5==0){
+        console.log("Buzz");
+    }
+    else{
+        console.log(i);
+    }
+}
+
